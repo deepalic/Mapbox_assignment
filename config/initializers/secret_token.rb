b00b693dc28eef5857a4c992cdabb45e2196ed1a
Deepali Chaudhary <deepalichaudhary06@gmail.com>
2015-12-11 23:34:15 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MpboxAssignment::Application.config.secret_key_base = 'f393e6054ea9cbdc10f9b49338d6b4af0dfbea6f70658a83d4f73cf52cb645fc86676567c35d32f0fac322b7f9a50d6ce91050d3edc4a92a5c9917c67ad8458a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MpboxAssignment::Application.config.session_store :cookie_store, key: '_mpbox_assignment_session'
